8fff0a8aa7288228591f6a14e0241916f3df318e
TxTony <t.tipveau@berd.re>
2020-08-08 10:48:42 +0400

    add arguments command

--- cli.js
+++ cli.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
-console.log("Hello world")
+const yargs = require('yargs');
+const sample = require('./src/sample.js');
+const argv = yargs
+    .command('hello', 'say hello and give time or not')
+    .option('time', {
+        alias: 't',
+        description: 'Tell the present Time',
+        type: 'boolean',
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+sample.run(argv);
